151cc2d8e823fa1554d3cf528ea222d141dd750c
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2021-01-21 23:05:40 +0100

    Project 52 parte 1.0 add and fix html php laravel

---
+++ resources/views/classrooms/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content-main')
+    <div class="container mb-5">
+        <h1>{{ $classroom->name }} details</h1>
+        <h6>ID: {{ $classroom->id }}</h6>
+        <hr>
+
+        <p>{{ $classroom->description }}</p>
+        <hr>
+
+        <div class="mb-3">Created at: {{ $classroom->created_at }}</div>
+        <div class="mb-3">Updated at: {{ $classroom->updated_at }}</div>
+    </div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content-main')
+    <div class="container mb-5">
+        <h1>SCHOOL HOME PAGE HERE</h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ex repellat dicta eius dignissimos nostrum maiores 
+           commodi suscipit, dolor non vitae sequi voluptatem praesentium sapiente quidem, labore necessitatibus esse quasi.</p>
+    </div>
+@endsection
