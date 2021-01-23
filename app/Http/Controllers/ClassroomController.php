78966b3feb69e34bfc673bf4c72d62021ea7d639
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2021-01-23 19:52:15 +0100

    Project 52 parte 1.2 fix html laravel

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Classroom;
+use Illuminate\Validation\Rule;
+use Carbon\Carbon;
 
 class ClassroomController extends Controller
 {
@@ -15,8 +17,29 @@ class ClassroomController extends Controller
     public function index()
     {
         // Get data from DB
-        $classrooms = Classroom::all();
+        // $classrooms = Classroom::all();
         // dd($classrooms);
+        $classrooms = Classroom::paginate(4);
+        
+        // CARBON 
+        // $dt = Carbon::now();
+        // $dt = Carbon::yesterday();
+        // dump($dt->toDateString());
+        // dump($dt->format('d/m/Y'));
+
+        //  Comparazione
+        // $first = Carbon::create('2021/01/13');
+        // $second = Carbon::create('2020/01/13');
+        // dump( $first->lt($second) );
+
+        // Differenza giorni
+        // $from = Carbon::create('2021/01/13');
+        // dump( $from->diffInDays() );
+        // dump( $from->diffInDays('2021/02/13') );
+
+        // $dt = Carbon::now()->locale('it_IT');
+        // dump( $dt->locale() );
+        // dump( $dt->isoFormat('dddd DD/MM/YYYY') );
 
         return view('classrooms.index', compact('classrooms'));
     }
@@ -51,8 +74,9 @@ class ClassroomController extends Controller
 
         // SAVE FROM DB
         $classroom = new Classroom();
-        $classroom->name = $data['name'];
-        $classroom->description = $data['description'];
+        // $classroom->name = $data['name'];
+        // $classroom->description = $data['description'];
+        $classroom->fill($data); // <-- $fillable nel model
 
         $saved = $classroom->save();
         // dd($saved);
@@ -78,9 +102,8 @@ class ClassroomController extends Controller
        return view('classrooms.show', compact('classroom'));
     }
 
+    // PARTE CRUD 2
 
-
-    
     /**
      * Show the form for editing the specified resource.
      *
@@ -89,7 +112,9 @@ class ClassroomController extends Controller
      */
     public function edit($id)
     {
-        //
+        $classroom = Classroom::find($id);
+
+        return view('classrooms.edit', compact('classroom'));
     }
 
     /**
@@ -101,7 +126,28 @@ class ClassroomController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // DATI INVIATI DALLA FORM
+        $data = $request->all();
+
+        // ISTANZA SPECIFICA
+        $classroom = Classroom::find($id);
+
+        // VALIDATION
+        $request->validate([
+            'name' => [
+                'required',
+                Rule::unique('classrooms')->ignore($id),
+                'max:10'
+            ], 
+            'description' => 'required'
+        ]);
+
+        // AGGIORNARE DATI DB
+        $updated = $classroom->update($data); // <-- $fillable nel model
+
+        if($updated) {
+            return redirect()->route('classrooms.show', $classroom->id);
+        }
     }
 
     /**
@@ -112,6 +158,13 @@ class ClassroomController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $classroom = Classroom::find($id);
+
+        $ref = $classroom->name;
+        $deleted = $classroom->delete();
+
+        if($deleted) {
+            return redirect()->route('classrooms.index')->with('deleted', $ref);
+        }
     }
 }
